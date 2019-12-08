9ac8df97863854c0b8362f6a416be990afdf84e9
sargis719 <58614963+sargis719@users.noreply.github.com>
2019-12-08 10:27:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def add_numbers_post():
   	      try:
   	      	for str_num in request.form['text'].split():
   	      		#total += int(str_num)
-			if str_num > total:
-				total = str_num
+			if int(str_num) > total:
+				total = int(str_num)
   	      	return render_template('add_numbers.html', result=str(total))
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
